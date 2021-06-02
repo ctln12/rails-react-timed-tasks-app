85a21d417bf64f3e4240141f47edf8dcb87e835e
Cataline <catalinebaert@yahoo.com>
2021-06-02 23:26:13 +0200

    Install fontawesome package, import it in index.jsx and use icons in Navbar component

--- app/javascript/components/Navbar/Navbar.jsx
+++ app/javascript/components/Navbar/Navbar.jsx
@@ -1,18 +1,15 @@
 import React from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCog } from '@fortawesome/free-solid-svg-icons';
-import { faBars } from '@fortawesome/free-solid-svg-icons';
 
 const Navbar = () => {
-  const settingsIcon = <FontAwesomeIcon icon={faCog} />
-  const menuIcon = <FontAwesomeIcon icon={faBars} />
   return (
     <div className="Navbar flex justify-between p-4 text-2xl h-16">
       <div className="Navbar-menu">
-        <span>{menuIcon}</span>
+        <i className="fas fa-bars"></i>
         <span className="ml-4 font-light text-xl">Timer</span>
       </div>
-      <div className="Navbar-settings">{settingsIcon}</div>
+      <div className="Navbar-settings">
+        <i className="fas fa-cog"></i>
+      </div>
     </div>
   );
 }
