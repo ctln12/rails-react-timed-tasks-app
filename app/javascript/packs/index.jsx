f5d7d2890d54d9c44ec399032dbd92854987c019
Cataline <catalinebaert@yahoo.com>
2021-06-03 00:19:42 +0200

    Move back stylesheets folder into app/javascript folder

--- app/javascript/packs/index.jsx
+++ app/javascript/packs/index.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from '../components/App';
 import '@fortawesome/fontawesome-free/css/all.min.css';
-import './stylesheets/application.scss';
+import '../stylesheets/application.scss';
 
 document.addEventListener('DOMContentLoaded', () => {
   ReactDOM.render(
