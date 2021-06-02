e74f52f343f0547acabc2df4526df8bfcc33f35d
Cataline <catalinebaert@yahoo.com>
2021-06-03 00:30:13 +0200

    Move import fontawesome

--- app/javascript/packs/index.jsx
+++ app/javascript/packs/index.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from '../components/App';
-import '@fortawesome/fontawesome-free/css/all.min.css';
 import '../stylesheets/application.scss';
 
 document.addEventListener('DOMContentLoaded', () => {
