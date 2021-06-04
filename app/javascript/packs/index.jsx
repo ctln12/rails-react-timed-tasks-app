5626c5041e72f8afde76f3e33963c4019dcf23ae
Cataline <catalinebaert@yahoo.com>
2021-06-04 15:54:01 +0200

    Import tailwindcss and fontawesome, change tag name from main back to div

--- app/javascript/packs/index.jsx
+++ app/javascript/packs/index.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from '../components/App';
+import 'tailwindcss/tailwind.css';
+import "@fortawesome/fontawesome-free/css/all";
 import '../stylesheets/application.scss';
+
+import App from '../components/App';
 
 document.addEventListener('DOMContentLoaded', () => {
   ReactDOM.render(
     <App />,
-    document.body.appendChild(document.createElement('main')),
+    document.body.appendChild(document.createElement('div')),
   )
 })
