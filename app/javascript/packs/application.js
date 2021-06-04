5f4355f4884bb022b764495017d8a97f38ed88c7
Cataline <catalinebaert@yahoo.com>
2021-06-04 15:56:24 +0200

    Remove import of tailwindcss and fontawesome

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -15,7 +15,3 @@ require("channels")
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
-
-require("stylesheets/application.scss")
-
-import "@fortawesome/fontawesome-free/css/all";
