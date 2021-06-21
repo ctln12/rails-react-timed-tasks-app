ce8d35a96f5f89660ee1ce2a58b8da73dfbdaf79
Cataline <catalinebaert@yahoo.com>
2021-06-21 14:37:50 +0200

    Refactor handleChange functions into one

--- app/javascript/components/Settings/Settings.jsx
+++ app/javascript/components/Settings/Settings.jsx
@@ -10,27 +10,17 @@ class Settings extends Component {
       longBreakLength: 15,
       longBreakAfter: 4
     }
-    this.handleSelectFocusSession = this.handleSelectFocusSession.bind(this);
-    this.handleSelectShortBreak = this.handleSelectShortBreak.bind(this);
-    this.handleSelectLongBreak = this.handleSelectLongBreak.bind(this);
-    this.handleSelectBreakAfter = this.handleSelectBreakAfter.bind(this);
+    this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
-  handleSelectFocusSession(event) {
-    this.setState({focusSessionLength: event.target.value});
+  handleChange(event) {
+    this.setState({[event.target.id]: event.target.value});
   }
-  handleSelectShortBreak(event) {
-    this.setState({shortBreakLength: event.target.value});
-  }
-  handleSelectLongBreak(event) {
-    this.setState({longBreakLength: event.target.value});
-  }
-  handleSelectBreakAfter(event) {
-    this.setState({longBreakAfter: event.target.value});
-  }
+
   handleSubmit(event) {
     alert('Saving new settings...');
+    // JSON.parse(localStorage.getItem('allProjects')) || []
     event.preventDefault();
   }
 
@@ -48,25 +38,41 @@ class Settings extends Component {
         <form onSubmit={this.handleSubmit}>
           <label className="block mb-8">
             Focus Session Length
-            <select className="block w-full mt-1 rounded border-gray-300 shadow-sm max-h-10" id="focus" value={this.state.focusSessionLength} onChange={this.handleSelectFocusSession}>
+            <select
+              className="block w-full mt-1 rounded border-gray-300 shadow-sm max-h-10"
+              id="focusSessionLength"
+              value={this.state.focusSessionLength}
+              onChange={this.handleChange}>
               {this.createSelectItems(240, 'minutes')}
             </select>
           </label>
           <label className="block mb-8">
             Short Break Length
-            <select className="block w-full mt-1 rounded border-gray-300 shadow-sm" value={this.state.shortBreakLength} onChange={this.handleSelectShortBreak}>
+            <select
+              className="block w-full mt-1 rounded border-gray-300 shadow-sm"
+              id="shortBreakLength"
+              value={this.state.shortBreakLength}
+              onChange={this.handleChange}>
               {this.createSelectItems(240, 'minutes')}
             </select>
           </label>
           <label className="block mb-8">
             Long Break Length
-            <select className="block w-full mt-1 rounded border-gray-300 shadow-sm" value={this.state.longBreakLength} onChange={this.handleSelectLongBreak}>
+            <select
+              className="block w-full mt-1 rounded border-gray-300 shadow-sm"
+              id="longBreakLength"
+              value={this.state.longBreakLength}
+              onChange={this.handleChange}>
               {this.createSelectItems(240, 'minutes')}
             </select>
           </label>
           <label className="block mb-8">
             Long Break After
-            <select className="block w-full mt-1 rounded border-gray-300 shadow-sm" value={this.state.longBreakAfter} onChange={this.handleSelectBreakAfter}>
+            <select
+              className="block w-full mt-1 rounded border-gray-300 shadow-sm"
+              id="longBreakAfter"
+              value={this.state.longBreakAfter}
+              onChange={this.handleChange}>
               {this.createSelectItems(60, 'focus sessions')}
             </select>
           </label>
